4fa587a7d63a2ea12d6e130afad6f2debc8d6423
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-10-18 15:43:56 -0500

    agregando componente de login falta arreglar el css

--- invitados/src/index.js
+++ invitados/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./componentes/Login/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // <Login></Login>
 );
 
 // If you want to start measuring performance in your app, pass a function
